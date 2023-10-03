fd520a341b616137e45e6bc0d69ea77bf06cc355
Felipe Tavares <felipe.tavares@outlook.com>
2023-10-03 17:26:35 -0300

    Add controller e class movie

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+
+namespace MovieApi.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+    }
+}
