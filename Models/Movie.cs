e238641bdea7df60ad49340b92972ee674bc847f
Felipe Tavares <felipe.tavares@outlook.com>
2023-10-03 17:44:18 -0300

    Adicionando datAannotations

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieApi.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "The movie title is required")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "The movie genre is required")]
+    [MaxLength(ErrorMessage = "The maxlength for genre field is 50 characters")]
     public string Genre { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "The duration must be between 70 and 600 minutes")]
     public int Duration { get; set; }
 }
