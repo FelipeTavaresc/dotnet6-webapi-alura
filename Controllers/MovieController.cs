9d8d1e005c6242da817deaed7b8530ce35a96b44
Felipe Tavares <felipe.tavares@outlook.com>
2023-10-04 17:37:55 -0300

    Add endpoint patch

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using MovieApi.Data;
 using MovieApi.Data.Dtos;
@@ -53,4 +54,22 @@ public class MovieController : ControllerBase
         _context.SaveChanges();
         return NoContent();
     }
+
+    [HttpPatch("{id}")]
+    public IActionResult UpdateMoviePatch(int id, JsonPatchDocument<UpdateMovieDto> patch)
+    {
+        var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+        if(movie == null)
+            return NotFound();
+
+        var movieToUpdate = _mapper.Map<UpdateMovieDto>(movie);
+        patch.ApplyTo(movieToUpdate, ModelState);
+        if(!TryValidateModel(movieToUpdate))
+            return ValidationProblem(ModelState);
+
+        _mapper.Map(movieToUpdate, movie);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
 }
